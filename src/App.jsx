898c8ace8e951da5503188048ad74386ace12484
Vitaliy <155250304+ShraM26@users.noreply.github.com>
2024-09-13 11:01:47 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
-import { Route, Routes } from 'react-router-dom';
-import HomePage from 'pages/HomePage/HomePage';
-import RegistrationPage from 'pages/RegistrationPage/RegistrationPage';
-import LoginPage from 'pages/LoginPage/LoginPage';
-import ContactsPage from 'pages/ContactsPage/ContactsPage';
-import PrivateRoute from 'components/PrivateRoute/PrivateRoute';
-import RestrictedRoute from 'components/RestrictedRoute/RestrictedRoute';
-import Layout from 'components/Layout/Layout';
+import React, { Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute';
+import './index.css';
 
-function App() {
-  return (
+const HomePage = React.lazy(() => import('./pages/HomePage/HomePage'));
+const ContactsPage = React.lazy(() => import('./pages/ContactsPage/ContactsPage'));
+const LoginPage = React.lazy(() => import('./pages/LoginPage/LoginPage'));
+const RegistrationPage = React.lazy(() => import('./pages/RegistrationPage/RegistrationPage'));
+
+const App = () => (
+  <Suspense fallback={<div>Loading...</div>}>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/register" element={<RestrictedRoute redirectTo="/contacts" component={<RegistrationPage />} />} />
-        <Route path="/login" element={<RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />} />
-        <Route path="/contacts" element={<PrivateRoute redirectTo="/login" component={<ContactsPage />} />} />
+        <Route
+          path="/contacts"
+          element={<PrivateRoute component={ContactsPage} redirectTo="/login" />}
+        />
+        <Route
+          path="/login"
+          element={<RestrictedRoute component={LoginPage} redirectTo="/contacts" />}
+        />
+        <Route
+          path="/register"
+          element={<RestrictedRoute component={RegistrationPage} redirectTo="/contacts" />}
+        />
       </Route>
     </Routes>
-  );
-}
+  </Suspense>
+);
 
 export default App;
