3859b4381ef074beef653401beb913e97eaf3bec
Vitaliy <155250304+ShraM26@users.noreply.github.com>
2024-09-13 10:37:24 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
-import { Layout } from './components/Layout/Layout';
-import { HomePage } from './pages/HomePage/HomePage';
-import { RegistrationPage } from './pages/RegistrationPage/RegistrationPage';
-import { LoginPage } from './pages/LoginPage/LoginPage';
-import { ContactsPage } from './pages/ContactsPage/ContactsPage';
-import { PrivateRoute } from './components/PrivateRoute/PrivateRoute';
-import { RestrictedRoute } from './components/RestrictedRoute/RestrictedRoute';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { refreshUser } from './redux/auth/operations';
-import { selectIsRefreshing } from './redux/auth/selectors';
+import HomePage from 'pages/HomePage/HomePage';
+import RegistrationPage from 'pages/RegistrationPage/RegistrationPage';
+import LoginPage from 'pages/LoginPage/LoginPage';
+import ContactsPage from 'pages/ContactsPage/ContactsPage';
+import PrivateRoute from 'components/PrivateRoute/PrivateRoute';
+import RestrictedRoute from 'components/RestrictedRoute/RestrictedRoute';
+import Layout from 'components/Layout/Layout';
 
 function App() {
-  const dispatch = useDispatch();
-  const isRefreshing = useSelector(selectIsRefreshing);
-
-  useEffect(() => {
-    dispatch(refreshUser());
-  }, [dispatch]);
-
-  return isRefreshing ? (
-    <p>Refreshing user...</p>
-  ) : (
+  return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route
-          path="/register"
-          element={<RestrictedRoute component={<RegistrationPage />} redirectTo="/contacts" />}
-        />
-        <Route
-          path="/login"
-          element={<RestrictedRoute component={<LoginPage />} redirectTo="/contacts" />}
-        />
-        <Route
-          path="/contacts"
-          element={<PrivateRoute component={<ContactsPage />} redirectTo="/login" />}
-        />
+        <Route path="/register" element={<RestrictedRoute redirectTo="/contacts" component={<RegistrationPage />} />} />
+        <Route path="/login" element={<RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />} />
+        <Route path="/contacts" element={<PrivateRoute redirectTo="/login" component={<ContactsPage />} />} />
       </Route>
     </Routes>
-  );
-}
-
-export default App;
-    </div>
   );
 }
 
