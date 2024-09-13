8066bf957dfd1bba5d2a1b5aa7c7a87b11fa082b
Vitaliy <155250304+ShraM26@users.noreply.github.com>
2024-09-13 09:46:40 +0300

    Update ContactsPage.jsx

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,35 +1,75 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchContacts } from '../../redux/contacts/operations';
-import { selectContacts } from '../../redux/contacts/slice';
-
-
-import ContactForm from '../../components/ContactForm/ContactForm';
-import Contact from '../../components/Contact/Contact'; 
-import SearchBox from '../../components/SearchBox/SearchBox';
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectUserContacts,
+  selectUserContactsError,
+  selectUserContactsIsLoading,
+} from "../redux/contacts/selectors";
+import Loader from "../components/Loader/Loader";
+import { useEffect } from "react";
+import {
+  apiAddNewContact,
+  apiDeleteContact,
+  apiGetAllContacts,
+} from "../redux/contacts/operations";
+import toast from "react-hot-toast";
+import AddContactForm from "../components/AddContactForm/AddContactForm";
 
 const ContactsPage = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
+  const contacts = useSelector(selectUserContacts);
+  const isLoading = useSelector(selectUserContactsIsLoading);
+  const error = useSelector(selectUserContactsError);
 
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(apiGetAllContacts())
+      .unwrap()
+      .then(() => {
+        toast.success("Contacts loaded successfully🎉");
+      });
   }, [dispatch]);
+
+  const onAddContact = (contact) => {
+    dispatch(apiAddNewContact(contact))
+      .unwrap()
+      .then(() => {
+        toast.success("Contact added successfully🎉");
+      });
+  };
+
+  const onDeleteContact = (contactId) => {
+    dispatch(apiDeleteContact(contactId))
+      .unwrap()
+      .then(() => {
+        toast.success("Contact deleted successfully🎉");
+      });
+  };
 
   return (
     <div>
-      <h1>Contacts</h1>
-       <SearchBox />
+      <AddContactForm onAddContact={onAddContact} />
+      {isLoading && <Loader />}
+      {error !== null && (
+        <p style={{ color: "red" }}>{error}. Please, try again later.</p>
+      )}
       <ul>
-        {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            name={contact.name}
-            number={contact.number}
-            id={contact.id}
-          />
-        ))}
+        {contacts?.length === 0 && <li>Contacts list is empty</li>}
+        {Array.isArray(contacts) &&
+          contacts.map((contact) => (
+            <li key={contact.id}>
+              <p>
+                <b>{contact.name}</b>: {contact.number}
+              </p>
+              <button onClick={() => onDeleteContact(contact.id)} type="button">
+                ❌
+              </button>
+            </li>
+          ))}
       </ul>
+    </div>
+  );
+};
+
+export default ContactsPage;
       <ContactForm />
     </div>
   );
