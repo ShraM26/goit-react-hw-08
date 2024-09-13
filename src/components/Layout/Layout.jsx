1e2419da96b1a95ff14494c59f3ca42b10fc9ae9
Vitaliy <155250304+ShraM26@users.noreply.github.com>
2024-09-13 10:38:03 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,15 @@
+import AppBar from 'components/AppBar/AppBar';
 import { Outlet } from 'react-router-dom';
-import { AppBar } from '../AppBar/AppBar';
 
-export const Layout = () => {
+const Layout = () => {
   return (
-    <div>
+    <>
       <AppBar />
       <main>
         <Outlet />
       </main>
-    </div>
+    </>
   );
 };
+
+export default Layout;
